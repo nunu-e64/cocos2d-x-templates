887bdd3721bbb95a999650a0f18b5d75278f37ec
Fumiya Amano <fumiya.amano@gree.co.jp>
2017-02-23 18:00:54 +0900

    add Ref to SubClass

---
+++ Cocos2d-x Class.xctemplate/Ref/___FILEBASENAME___.h
@@ -0,0 +1,21 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#pragma once
+
+class ___FILEBASENAMEASIDENTIFIER___ : public cocos2d::___VARIABLE_cocos2dxSubclass___
+{
+public:
+    CREATE_FUNC(___FILEBASENAMEASIDENTIFIER___);
+
+private:
+    bool init() override;
+    void onEnter() override;
+    void onExit() override;
+
+};
